4693716c7f38b34bd748a78df60432d2cb39d3e0
Nicolas Hallaert <nicolas.hallaert.pro@gmail.com>
2018-09-11 10:56:30 +0200

    a

--- prod.php
+++ prod.php
@@ -2,67 +2,69 @@
 
 <?php
 
-$prodlist = array 
-(
-  'prod1' => array 
-  (
-    'name' => 'Lebron 11 XI',
-    'price' => '€69.99',
-    'dispo' => 'Il ne reste pu que deux exemplaires !',
-    'size' => '43EU','41EU',
-    'color' => 'Black',
-    'desc' => 'Synthétique',
-  ),
-    'prod2' => array 
-  (
-    'name' => 'LeBron 15',
-    'price' => '€150',
-    'dispo' => 'Dernier exemplaire disponible',
-    'size' => '40EU',
-    'color' => 'Multicolor/black',
-    'desc' => 'Synthétique',
-  ),
-    'prod3' => array 
-  (
-    'name' => 'High Top-G0621_2',
-    'price' => '€67.30',
-    'dispo' => 'None',
-    'size' => 'None',
-    'color' => 'Red',
-    'desc' => 'Breathable',
-  ),
-    'prod4' => array 
-  (
-    'name' => 'Air Jordan 1',
-    'price' => '€109.99',
-    'dispo' => 'Il reste deux exemplaires !',
-    'size' => '44.5', '45',
-    'color' => 'Red',
-    'desc' => 'Leather',
-  )
-);
+$prodlist = array
+    (
+      'prod1' => array
+      (
+        'name' => 'Lebron 11 XI',
+        'price' => '€69.99',
+        'picture' => '<img name ="Lebron 11 XI" method="post" action="prod.php" class="size-img" src="img/ankle-men-s-air-cushion-authentic.jpg_640x640.jpg" alt=""',
+        'dispo' => 'Il ne reste pu que deux exemplaires !',
+        'size' => '43EU','41EU',
+        'color' => 'Black',
+        'desc' => 'Synthétique',
+      ),
+        'prod2' => array
+      (
+        'name' => 'LeBron 15',
+        'price' => '€150',
+        'picture' => '<img name ="LeBron 15" class="size-img" src="img/nike-zoom-kobe-10.jpg" alt=""',
+        'dispo' => 'Dernier exemplaire disponible',
+        'size' => '40EU',
+        'color' => 'Multicolor/black',
+        'desc' => 'Synthétique',
+      ),
+        'prod3' => array
+      (
+        'name' => 'High Top-G0621_2',
+        'price' => '€67.30',
+        'picture' => '<img name ="High Top-G0621_2" class="size-img" src="img/product-image-563631783.jpg" alt=""',
+        'dispo' => 'None',
+        'size' => 'None',
+        'color' => 'Red',
+        'desc' => 'Breathable',
+      ),
+        'prod4' => array
+      (
+        'name' => 'Air Jordan 1',
+        'price' => '€109.99',
+        'picture' => '<img name ="Air Jordan 1" class="size-img" src="img/Sneakers-High-Top.jpg" alt=""',
+        'dispo' => 'Il reste deux exemplaires !',
+        'size' => '44.5', '45',
+        'color' => 'Red',
+        'desc' => 'Leather',
+      )
+    );
+?>
 
 
-    include 'header.html';
-  ?>
+    <?php
+      include 'header.html';
+    ?>
+    
+    <section>
+      <div class="container">
+        <div class="col-10 offset-1 prod" style="border:1px solid red">
 
-  <div class="container-fluid">
-    <div class="col-10 offset-1" style="border:1px solid red; height:88vh;">
+          <?php 
+            $prodlist[$_GET['index']]['name'];
+          ?>
 
-
-
-
-<!-- <?php 
-foreach ($prodlist as $prod) {
-  foreach ($prod as $element) {
-  echo $element . "<br />";
-  };
-}; ?> -->
-
-    </div>
-  </div>
-
-  <?php
-    include 'footer.html';
-  ?>
+        </div>
+      </div>
+    </section>
+  
+    <?php
+      include 'footer.html';
+    ?>
 
