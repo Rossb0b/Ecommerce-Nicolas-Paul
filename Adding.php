0d3de3b9442c2545fdffae568bf2571e77841e97
Nicolas Hallaert <nicolas.hallaert.pro@gmail.com>
2018-09-17 10:52:42 +0200

    added verification admin logs

--- Adding.php
+++ Adding.php
@@ -2,3 +2,26 @@
 
 session_start();
 
+
+?>
+
+<form method="post" action="Adding.php">
+	<input type="text" name="identifiant"/>
+	<input type="password" name="password"/>
+	<button type="submit">se connecter</button>
+</form>
+
+<?php 
+
+if (isset($_POST['identifiant']) AND isset($_POST['password'])) {
+		if ($_POST['identifiant'] == $_SESSION['identifiant'] AND $_POST['password'] == $_SESSION['password']) 
+		{
+			echo $_POST['identifiant'];
+		}
+		else 
+		{
+			echo "Veuillez entrer un identifiant valide";
+		}
+	}
+
+?>
